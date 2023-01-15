f45ea806d4b47346aa1923bf1c8e95f104979447
lynxful <111362048+lynxful@users.noreply.github.com>
2023-01-15 15:54:27 +0200

    Add files via upload

---
+++ CountInString.cs
@@ -0,0 +1,32 @@
+﻿namespace CountInString
+{
+    internal class CountInString
+    {
+        static public void Main()
+        {
+            String line;
+            line = Console.ReadLine();
+            int T = Convert.ToInt32(line);
+
+            for (int t_i = 0; t_i < T; t_i++)
+            {
+                String S;
+                S = Console.ReadLine();
+                line = Console.ReadLine();
+                char k = Convert.ToChar(line);
+
+                int out_ = solve(S, k);
+                Console.Out.WriteLine(out_);
+
+            }
+        }
+
+        static int solve(String S, char k)
+        {
+            int result = 0;
+            foreach (char c in S.ToCharArray()) if(c == k) result++;
+            return result;
+
+        }
+    }
+}

69e1ba21474038829db284ad15232e5bddabf8f6
lynxful <111362048+lynxful@users.noreply.github.com>
2023-01-15 14:22:51 +0200

    Add files via upload

---
+++ Zoo.cs
@@ -0,0 +1,43 @@
+﻿using System.Diagnostics;
+using System.Reflection.PortableExecutable;
+
+namespace Zoo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Word:");
+            string word = Console.ReadLine();
+            Console.Clear();
+            if (similarity(word) == true) Console.WriteLine("YES");
+            else Console.WriteLine("NO");
+
+        }
+
+        public static bool similarity(string s)
+        {
+            int x = 0;
+            int y = 0;
+
+            for(int i = 0; i < s.Length;i++)
+            {
+                string character = s.Substring(i,1);
+
+                switch (character)
+                {
+                    case "z":
+                        x++;
+                        break;
+                    case "o":
+                        y++;
+                        break;
+                }
+            }
+
+            if (x * 2 == y) return true; 
+            else return false;
+             
+        }
+    }
+}
